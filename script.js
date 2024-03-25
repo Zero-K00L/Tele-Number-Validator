ddc503404c571e110f37e96864f07e670ae2e197
John Grudzinski <johngrudzinski@yahoo.com>
2024-03-25 15:09:25 -0400

    JS updated. Valid and Invalid messages are working. Alert message still needs work to trigger on just empty strings

---
+++ script.js
@@ -0,0 +1,55 @@
+const input = document.getElementById('user-input');
+const results = document.getElementById('results-list');
+const checkBtn = document.getElementById('check-btn');
+const clearBtn = document.getElementById('clear-btn');
+
+const numberRegex = /(1-)?\d{3}(-|\s)?\d{3}(-|\s)?\d{4}/;
+
+const validNumber = (input) => numberRegex.test(input.value);
+
+/* const alertUser = () => {
+    const userInput = input.value;
+    if (userInput === '') {
+        alert('Please provide a phone number');
+        return false;
+    }
+    return true;
+} */
+
+const displayResult = () => {
+    const userInput = input.value;
+    if(userInput === '') {
+        alert('Please provide a phone number');
+        return;
+    }
+
+    else if(validNumber(input) === true) {
+        const userInput = input.value;
+        const liElement = document.createElement('li');
+        liElement.textContent = `Valid US number: ${userInput}`;
+        results.appendChild(liElement);
+        liElement.classList.add('valid');
+        input.value = '';
+        return;
+    }
+    else {
+        const userInput = input.value;
+        const liElement = document.createElement('li');
+        liElement.textContent = `Invalid US number: ${userInput}`;
+        results.appendChild(liElement);
+        liElement.classList.add('invalid');
+        input.value = '';
+        return;
+    }
+}
+
+
+const clearResults = () => {
+    Array.from(results.children).forEach(li => {
+        li.remove();
+    });
+}
+
+
+checkBtn.addEventListener('click', displayResult);
+clearBtn.addEventListener('click', clearResults);
