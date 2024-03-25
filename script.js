619025678515749593ec1e53442f9cc2a767cd70
John Grudzinski <johngrudzinski@yahoo.com>
2024-03-25 15:42:16 -0400

    Updated Regex to account for different cases. Left old ones commented out for testing purposes. Changed where results were displayed to comply with requirements

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const input = document.getElementById('user-input');
-const results = document.getElementById('results-list');
+const results = document.getElementById('results-div');
 const checkBtn = document.getElementById('check-btn');
 const clearBtn = document.getElementById('clear-btn');
 
-const numberRegex = /(1-)?\d{3}(-|\s)?\d{3}(-|\s)?\d{4}/;
-const validNumber = (input) => numberRegex.test(input.value);
+/* const numberRegex = /(1-)?\d{3}(-|\s)?\d{3}(-|\s)?\d{4}/; */
+/* const numberRegex = /(1-)?[0-9]{3}(-|\s)?[0-9]{3}(-|\s)?[0-9]{4}/; */
 
+const numberRegex = /^(1\s?)?(\(\d{3}\)|\d{3})(-|\s)?\d{3}(-|\s)?\d{4}$/;
+const validNumber = (input) => numberRegex.test(input.value);
 const displayResult = () => {
     const userInput = input.value;
     if(userInput === '') {
@@ -14,29 +16,21 @@ const displayResult = () => {
     }
     else if(validNumber(input) === true) {
         const userInput = input.value;
-        const liElement = document.createElement('li');
-        liElement.textContent = `Valid US number: ${userInput}`;
-        results.appendChild(liElement);
-        liElement.classList.add('valid');
+        const divResults = results;
+        divResults.textContent = `Valid US number: ${userInput}`;
         input.value = '';
         return;
     }
     else {
         const userInput = input.value;
-        const liElement = document.createElement('li');
-        liElement.textContent = `Invalid US number: ${userInput}`;
-        results.appendChild(liElement);
-        liElement.classList.add('invalid');
+        const divResults = results;
+        divResults.textContent = `Invalid US number: ${userInput}`;
         input.value = '';
         return;
     }
 }
 
-const clearResults = () => {
-    Array.from(results.children).forEach(li => {
-        li.remove();
-    });
-}
+const clearResults = () => results.textContent = '';
 
 checkBtn.addEventListener('click', displayResult);
 clearBtn.addEventListener('click', clearResults);
